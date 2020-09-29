d102e99e995ac9582bf6f55ba26508b72810c0e6
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-09-29 22:45:19 +0200

    New test

--- src/test/java/no/kristiania/httpclientdemo2/QuerryStringTest.java
+++ src/test/java/no/kristiania/httpclientdemo2/QuerryStringTest.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QuerryStringTest {
     @Test
-    void shouldRetrieveQueryParameter(){
+    void shouldRetrieveQueryParameter() {
         QuerryString querryString = new QuerryString("status=200");
         assertEquals("200", querryString.getParameter("status"));
     }
 
+    @Test
+    void shouldRetrieveOtherQueryParameter() {
+        QuerryString querryString = new QuerryString("status=404");
+        assertEquals("404", querryString.getParameter("status"));
+
+    }
 }
