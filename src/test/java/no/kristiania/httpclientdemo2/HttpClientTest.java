ac358e3519dc8c9f76f035c2f79573d1a6254e10
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-10-07 16:43:41 +0200

    HttpClientTest.shouldReadSuccessStatusCode red

---
+++ src/test/java/no/kristiania/httpclientdemo2/HttpClientTest.java
@@ -0,0 +1,15 @@
+package no.kristiania.httpclientdemo2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClientTest {
+
+    @Test
+    void shouldReadSuccessStatusCode() {
+        HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
+        assertEquals(200, httpClient.getResponceCode());
+    }
+
+}
