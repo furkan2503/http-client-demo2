ab8e8dc4c9b47c79acb187d6fff5be9d42915cd6
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-10-07 14:36:59 +0200

    Fix Maven build

--- src/main/java/no/kristiania/httpclientdemo2/QuerryString.java
+++ src/main/java/no/kristiania/httpclientdemo2/QuerryString.java
@@ -1,12 +1,23 @@
 package no.kristiania.httpclientdemo2;
 
-public class QuerryString {
-    public QuerryString(String queryString){
+import java.util.HashMap;
+import java.util.Map;
 
+public class QuerryString {
+    private Map<String, String> parameters = new HashMap<>();
+
+    public QuerryString(String querryString){
+        for (String parameter : querryString.split("&")) {
+            int equalsPos = parameter.indexOf('=');
+            String value = parameter.substring(equalsPos+1);
+            String parameterName = parameter.substring(0, equalsPos);
+            parameters.put(parameterName, value);
+
+        }
     }
 
     public String getParameter(String name) {
-        return "200";
+        return parameters.get(name);
     }
 }
 
